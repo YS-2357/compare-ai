638a34b788a9a13fa7f612399f245969ffc39ac0
YS-2357 <joungyoungsun20@gmail.com>
2025-12-24 19:54:25 +0900

    refactor: 서비스 import 경로 수정

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,6 @@
 """서비스 계층 패키지."""
 
-from .chat_graph import stream_chat
-from .prompt_eval import stream_prompt_eval
+from .chat_compare import stream_chat
+from .prompt_compare import stream_prompt_eval
 
 __all__ = ["stream_chat", "stream_prompt_eval"]
